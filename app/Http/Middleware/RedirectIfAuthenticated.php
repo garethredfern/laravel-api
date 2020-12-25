8f6b4290ca5116ff19fd311445529a68e0b50fdc
Gareth Redfern <garethr@hey.com>
2020-12-25 20:49:53 +0000

    send json if expected RedirectIfAuthenticated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,10 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+              if ($request->expectsJson()) {
+                return response()->json(['error' => 'Already authenticated.'], 400);
+              }
+              return redirect(RouteServiceProvider::HOME);
             }
         }
 
