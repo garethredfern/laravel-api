8cca46bf89cee71c5b8532e700477b4a34e558da
Gareth Redfern <garethr@hey.com>
2020-12-26 23:16:08 +0000

    redirect if auth now sends a 200 with message

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -24,7 +24,7 @@ class RedirectIfAuthenticated
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
               if ($request->expectsJson()) {
-                return response()->json(['error' => 'Already authenticated.'], 400);
+                return response()->json(['message' => 'Already authenticated.'], 200);
               }
               return redirect(RouteServiceProvider::HOME);
             }
