44752bf048b1b19222731241421510e054418692
Gareth Redfern <gareth@garethredfern.com>
2021-01-07 21:53:17 +0000

    add a token controller & endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TokenController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/sanctum/token', TokenController::class);
+
 Route::middleware('auth:sanctum')->get('/users/{user}', function (Request $request) {
   return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('/users', function (Request $request) {
+  return User::all();
 });
 
 Route::middleware('auth:sanctum')->get('/users/auth', function (Request $request) {
