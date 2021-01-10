3670690c6df01dfa468534bd1428c49b83a0a95a
Gareth Redfern <gareth@garethredfern.com>
2021-01-10 20:58:17 +0000

    add artisan command to clear logs

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('logs:clear', function () {
+    exec('rm ' . storage_path('logs/*.log'));
+    $this->comment('Logs have been cleared!');
+})->describe('Clear log files');
