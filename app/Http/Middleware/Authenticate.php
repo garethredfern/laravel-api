c7595bbf9dafe2a114fd4557287c300324b94735
Gareth Redfern <gareth@garethredfern.com>
2022-02-11 15:58:22 +0000

    update authentication middleware to laravel 9 syntax

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return url(env('SPA_URL') . '/login');
+            return route('login');
         }
     }
 }
