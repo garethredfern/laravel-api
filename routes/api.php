ed32f0717b0fedc680fd706a898635739e4852d3
Gareth Redfern <gareth@garethredfern.com>
2022-02-11 20:03:03 +0000

    update routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\TokenController;
 use App\Http\Controllers\AvatarController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\Auth\TokenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,15 +18,21 @@ use App\Http\Controllers\MessageController;
 |
 */
 
-Route::post('/sanctum/token', TokenController::class);
+Route::post('/sanctum/token/login', TokenController::class);
 
-Route::middleware(['auth:sanctum'])->group(function () {
-  Route::get('/users/auth', AuthController::class);
-  Route::get('/users/{user}', [UserController::class, 'show']);
-  Route::get('/users', [UserController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-  Route::post('/users/auth/avatar', [AvatarController::class, 'store']);
+    Route::get('/user/{user}', [UserController::class, 'show']);
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/user/avatar', AvatarController::class);
 
-  Route::post('/messages', [MessageController::class, 'store']);
-  Route::get('/messages', [MessageController::class, 'index']);
+    Route::post('/messages', [MessageController::class, 'store']);
+    Route::get('/messages', [MessageController::class, 'index']);
+
+    Route::post('/sanctum/token/logout', function (Request $request) {
+        $request->user()->tokens()->delete();
+    });
 });
