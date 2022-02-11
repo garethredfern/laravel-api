4011461045772c08107d6247e9fadd3a1c36ee49
Gareth Redfern <gareth@garethredfern.com>
2022-02-11 16:36:08 +0000

    delete messages on user delete

--- database/migrations/2022_02_11_155216_create_messages_table.php
+++ database/migrations/2022_02_11_155216_create_messages_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('body');
             $table->timestamps();
         });
