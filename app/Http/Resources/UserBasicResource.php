57b296fd285136a3d71ea26bead996ad6c112b5b
Gareth Redfern <gareth@garethredfern.com>
2022-02-11 15:50:57 +0000

    update resources with laravel 9 comments

--- app/Http/Resources/UserBasicResource.php
+++ app/Http/Resources/UserBasicResource.php
@@ -10,14 +10,14 @@ class UserBasicResource extends JsonResource
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return array
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-      return [
-        'id' => $this->id,
-        'name' => $this->name,
-        'email' => $this->email,
-      ];
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
     }
 }
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -10,17 +10,17 @@ class UserResource extends JsonResource
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return array
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
-          'id' => $this->id,
-          'name' => $this->name,
-          'email' => $this->email,
-          'avatar' => $this->avatar,
-          'isAdmin' => $this->isAdmin(),
-          'emailVerified' => $this->email_verified_at,
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'avatar' => $this->avatar,
+            'isAdmin' => $this->isAdmin(),
+            'emailVerified' => $this->email_verified_at,
         ];
     }
 }
