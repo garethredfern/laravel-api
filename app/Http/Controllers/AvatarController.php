0fbc5b26ba4040f430a477c7b4d67e12475706f7
Gareth Redfern <gareth@garethredfern.com>
2022-02-11 16:32:46 +0000

    update avatar controller for local storage

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -3,24 +3,23 @@
 namespace App\Http\Controllers;
 
 use Exception;
+use Illuminate\Http\File;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-
 class AvatarController extends Controller
 {
     public function store(Request $request)
     {
-      try {
-          $user = Auth::user();
-          $filePath = Storage::disk('spaces')
-              ->putFile('avatars/user-'.$user->id, $request->file, 'public');
-          $user->avatar = env('DO_SPACES_PUBLIC').$filePath;
-          $user->save();
-      } catch (Exception $exception) {
-          return response()->json(['message' => $exception->getMessage()], 409);
-      }
-          return new UserResource($user);
+        try {
+            $user = $request->user();
+            $filePath = Storage::disk('local')
+                ->putFile('avatars', new File($request->file), 'public');
+            $user->avatar = env('AWS_ENDPOINT').$filePath;
+            $user->save();
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 409);
+        }
+        return new UserResource($user);
     }
 }
