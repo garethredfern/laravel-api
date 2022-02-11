82f97d7461dcc1b0606c15eee950b0bc719062be
Gareth Redfern <gareth@garethredfern.com>
2022-02-11 15:20:42 +0000

    move token controller to auth folder

---
+++ app/Http/Controllers/Auth/TokenController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class TokenController extends Controller
+{
+  public function __invoke(Request $request)
+  {
+    $request->validate([
+      'email' => 'required|email',
+      'password' => 'required',
+      'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+      throw ValidationException::withMessages([
+        'email' => ['The provided credentials are incorrect.'],
+      ]);
+    }
+
+    $token = $user->createToken($request->device_name)->plainTextToken;
+
+    return response()->json(['token' => $token], 200);
+  }
+}
