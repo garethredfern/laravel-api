87752fb63f5d1ba836e27b7984ce4360a96d683b
Gareth Redfern <gareth@garethredfern.com>
2022-02-18 09:44:35 +0000

    add id to basic user resource

--- app/Http/Resources/UserBasicResource.php
+++ app/Http/Resources/UserBasicResource.php
@@ -15,6 +15,7 @@ class UserBasicResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'isAdmin' => $this->isAdmin(),
