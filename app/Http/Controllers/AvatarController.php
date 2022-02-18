89575d84e5b7e9a5a4e9d39616d3db86213e2820
Gareth Redfern <gareth@garethredfern.com>
2022-02-18 16:59:04 +0000

    return a 200 response with message when updating an avatar

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -4,19 +4,24 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
-use App\Http\Resources\UserResource;
 class AvatarController extends Controller
 {
+    /**
+     * Update the user's avatar.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function __invoke(Request $request)
     {
         try {
             $user = $request->user();
-            $path = $request->file('avatar')->storePublicly('public/avatars');
+            $path = $request->file('file')->storePublicly('public/avatars');
             $user->avatar = $path;
             $user->save();
         } catch (Exception $exception) {
             return response()->json(['message' => $exception->getMessage()], 409);
         }
-        return new UserResource($user);
+        return response()->json(["message" => "Avatar updated"], 200);
     }
 }
